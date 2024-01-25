e22a60e6d72864380eed531e8c1f541d77331bde
Vinicius-Tavares-Silva <vini_tavares10@hotmail.com>
2024-01-25 12:32:29 -0300

    create ModalLogin and auth services

---
+++ src/utils/validators.js
@@ -0,0 +1,26 @@
+const EMAIL_REGEX = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+
+export function validateEmptyAndLength3(value) {
+  if (!value) {
+    return '*Este campo é obrigatório'
+  }
+
+  if (value.length < 3) {
+    return '*Este campo precisa de no mínimo 3 caracteres'
+  }
+
+  return true
+}
+
+export function validateEmptyAndEmail(value) {
+  if (!value) {
+    return '*Este campo é obrigatório'
+  }
+
+  const isValid = EMAIL_REGEX.test(value)
+  if (!isValid) {
+    return '*Este campo precisa ser um e-mail'
+  }
+
+  return true
+}
