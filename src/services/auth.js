e4d64fa1b656070e43c5c4cccf41a37d49c9b194
Vinicius-Tavares-Silva <vini_tavares10@hotmail.com>
2024-02-03 13:19:02 -0300

    create ModalCreateAccount and register services

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,25 @@
 export default httpClient => ({
+  register: async({ name, email, password }) => {
+    const response = await httpClient.post('/auth/register', {
+      name,
+      email,
+      password,
+    })
+    let errors = null
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      }
+    }
+
+    return {
+      errors,
+      data: response.data,
+    }
+  },
+
   login: async({ email, password }) => {
     const response = await httpClient.post('/auth/login', {
       email,
