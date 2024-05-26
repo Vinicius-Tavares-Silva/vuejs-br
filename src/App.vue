e8aa8578e8dd960be1f22d53a0da711d92f445bf
Vinicius-Tavares-Silva <vini_tavares10@hotmail.com>
2024-05-26 16:08:51 -0300

    create route credentials check

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,29 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import services from './services'
 import ModalFactory from './components/ModalFactory'
 
 export default {
   components: {
     ModalFactory,
+  },
+  setup() {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async() => {
+      if (route.meta.hasAuth) {
+        const token = localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+        }
+        const { data } = await services.users.getMe()
+        console.log('data', data)
+      }
+    })
   }
 }
 </script>
